4c372006f8a1f35261e057372a18be57a7a1d58a
Marwan Hisham <marwanhiisham@outlook.com>
2023-06-18 15:25:04 +0300

    navbar not done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Navbar from "@/components/Navbar";
 import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
 import { Inter } from "next/font/google";
@@ -22,7 +23,10 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
-        {children}
+        <Navbar />
+        <div className="container max-w-7xl mx-auto h-full pt-12">
+          {children}
+        </div>
       </body>
     </html>
   );
