3cb090f3926bb731a255519201a2947e1ddc28af
Marwan Hisham <marwanhiisham@outlook.com>
2023-06-19 12:44:04 +0300

    creating user account menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Icons } from "./Icons";
 import { buttonVariants } from "./ui/Button";
 import { getAuthSession } from "@/lib/auth";
+import UserAccountNav from "./UserAccountNav";
 
 const Navbar = async ({}) => {
   const session = await getAuthSession();
@@ -16,8 +17,8 @@ const Navbar = async ({}) => {
           </p>
         </Link>
         {/* Searchbar */}
-        {session ? (
-          <p>You are logged in</p>
+        {session?.user ? (
+          <UserAccountNav user={session.user} />
         ) : (
           <Link href="/sign-in" className={buttonVariants()}>
             Sign In
---
+++ src/components/UserAvatar.tsx
@@ -0,0 +1,35 @@
+import { User } from "next-auth";
+import { FC } from "react";
+import { Avatar, AvatarFallback } from "./ui/Avatar";
+import Image from "next/image";
+import { Icons } from "./Icons";
+import { AvatarProps } from "@radix-ui/react-avatar";
+
+interface UserAvatarProps extends AvatarProps {
+  // Pick => allows us to pick specific properties from model type
+  user: Pick<User, "name" | "image">;
+}
+
+const UserAvatar: FC<UserAvatarProps> = ({ user, ...props }) => {
+  return (
+    <Avatar {...props}>
+      {user.image ? (
+        <div className="relative aspect-square h-full w-full">
+          <Image
+            fill
+            src={user.image}
+            alt="Profile Picture"
+            referrerPolicy="no-referrer"
+          />
+        </div>
+      ) : (
+        <AvatarFallback>
+          <span className="sr-only">{user.name}</span>
+          <Icons.user />
+        </AvatarFallback>
+      )}
+    </Avatar>
+  );
+};
+
+export default UserAvatar;
